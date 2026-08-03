f4c9898c6c2a16fff55bd1ece7c826841db5ce51
krsharathrao <krsharathrao@gmail.com>
2026-08-03 14:16:59 +0530

    Create variables.tf

---
+++ terraform/infrastructure/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "The name of the project, used for resource naming and tagging"
+  type        = string
+  default     = "aws-terraform-lab"
+}
+
+variable "environment" {
+  description = "Environment identifier (e.g. dev, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "bucket_name_prefix" {
+  description = "Prefix for the S3 bucket name"
+  type        = string
+  default     = "integrationninjas"
+}
